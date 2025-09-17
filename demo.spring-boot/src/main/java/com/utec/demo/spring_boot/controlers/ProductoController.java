e87e0c8cb3185f217682206606b914a8daff2d7d
Humberto Lozada <hb.lozadas@gmail.com>
2025-09-17 00:02:40 -0500

    Acionado una lista de productos y refactorizamos el adicionar producto, obtener producto y eliminar producto

--- demo.spring-boot/src/main/java/com/utec/demo/spring_boot/controlers/ProductoController.java
+++ demo.spring-boot/src/main/java/com/utec/demo/spring_boot/controlers/ProductoController.java
@@ -2,31 +2,54 @@ package com.utec.demo.spring_boot.controlers;
 
 import com.utec.demo.spring_boot.Producto.Producto;
 import com.utec.demo.spring_boot.Producto.ProductoDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("/api/productos")
 
 public class ProductoController {
+    private static final List<Producto> BD = new ArrayList<>();
+    private static final AtomicLong NEXT_ID = new AtomicLong (1L);
+
     @GetMapping
     public List<Producto> listarProductos()
     {
-        return List.of (
-                new Producto(1L, "Producto A", 20.0),
-                new Producto(2L, "Producto B", 30.0),
-                new Producto(3L, "Producto C", 40.0)
-        );
+        return BD;
     }
     @PostMapping
     public Producto crearProducto(@RequestBody ProductoDTO productoDto) {
-        System.out.println("Producto recibido: " + productoDto.getNombre() + ", Precio: " + productoDto.getPrecio());
-        return new Producto(4L, productoDto.getNombre(), productoDto.getPrecio());
+        Producto nuevoProducto = new Producto(
+                                    NEXT_ID.getAndIncrement(),
+                                    productoDto.getNombre(),
+                                    productoDto.getPrecio());
+        BD.add(nuevoProducto);
+        return nuevoProducto;
     }
     @GetMapping("/{id}")
     public Producto obtenerProducto(@PathVariable Long id)
     {
-        return new Producto(1L, "Producto A", 20.0);
+        return BD.stream()
+                        .filter(p -> p.getId().
+                        equals(id))
+                        .findFirst()
+                        .orElse(null);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> eliminarProducto(@PathVariable Long id)
+    {
+       Boolean notaEliminada = BD.removeIf(p -> p.getId().equals(id));
+        if (notaEliminada)
+        {
+            return ResponseEntity.noContent().build();
+        }
+        else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
